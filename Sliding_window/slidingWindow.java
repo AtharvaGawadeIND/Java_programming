80696dff752502f42c4fb6165c79d1ead937a063
Atharva Gawade <atharvagawade.in@gmail.com>
2026-09-25 20:49:32 +0530

    Add first negative number in window

--- Sliding_window/slidingWindow.java
+++ Sliding_window/slidingWindow.java
@@ -1,3 +1,5 @@
+
+
 public class slidingWindow{
 
   public static int maxSum(int arr[] , int k) {
@@ -13,10 +15,26 @@ public class slidingWindow{
 
       if(right - left +1 == k)
       {
-        
+
+        maxSum = Math.max(maxSum , sum);
+
+        sum = sum + arr[left];
+        left ++;
       }
     }
 
+
     return maxSum ;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] arr = {2,3,1,5,6,2};
+    int k = 3;
+
+    int ians = maxSum(arr , k);
+
+
+    System.out.println("Maximum sum is "+ ians);
   }
 }
